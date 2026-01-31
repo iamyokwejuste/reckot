ea8cd2f7103601ebb599f0a9f24d28138464745c
Steve Yonkeu <yokwejuste@yahoo.com>
2026-01-31 01:54:47 +0100

    fix: update static files storage backend to use CompressedManifestStaticFilesStorage
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- reckot/settings.py
+++ reckot/settings.py
@@ -212,7 +212,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 WHITENOISE_AUTOREFRESH = DEBUG
